544dd4615198bbc696b67150dfd750d9671ac5bf
David <davidyoungtimes@gmail.com>
2023-09-13 14:55:23 +0200

    Añadido services/galery,creacion de funciones get vacias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
     path: 'galeria-imagenes-basica',
     loadChildren: () => import('./pages/galeria-imagenes-basica/galeria-imagenes-basica.module').then( m => m.GaleriaImagenesBasicaPageModule)
   },
+
 ];
 
 @NgModule({
